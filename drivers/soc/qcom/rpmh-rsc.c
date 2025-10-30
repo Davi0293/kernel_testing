475c521c7b33eaf0692faa46b33a92daee625528
Greg Kroah-Hartman <gregkh@google.com>
2025-10-30 07:52:10 +0000

    Linux 5.4.301

--- drivers/soc/qcom/rpmh-rsc.c
+++ drivers/soc/qcom/rpmh-rsc.c
@@ -294,13 +294,11 @@ static irqreturn_t tcs_tx_done(int irq, void *p)
 		ipc_log_string(drv->ipc_log_ctx,
 			       "IRQ response: m=%d err=%d", i, err);
 
-		/*
-		 * if wake tcs was re-purposed for sending active
-		 * votes, clear AMC trigger & enable modes and
+		/* Clear AMC trigger & enable modes and
 		 * disable interrupt for this TCS
 		 */
-		if (!drv->tcs[ACTIVE_TCS].num_tcs) {
-			__tcs_trigger(drv, i, false);
+		__tcs_trigger(drv, i, false);
+
 			/*
 			 * Disable interrupt for this TCS to avoid being
 			 * spammed with interrupts coming when the solver
