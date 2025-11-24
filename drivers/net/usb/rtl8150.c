72b5232cb2f251f5f7b45ec932385d70c7a34ae4
Thomas Turner <thomas_turner@talktalk.net>
2025-11-24 18:44:29 +0000

    fixup! net: usb: rtl8150: Fix frame padding
    Fixes commit d4000407ca9b94779b86567198f74a3b34d3c2fc upstream.
    
    The fix was based on commit 2f5281ba2a8feaf6f0aee93356f350855bb530fc
    upstream.
    
    /home/thomas/a21s/kernel/qcom/sm8250/drivers/net/usb/rtl8150.c:707:10: error: comparison of distinct pointer types ('typeof (skb->len) *' (aka 'unsigned int *') and 'typeof (60) *' (aka 'int *')) [-Werror,-Wcompare-distinct-pointer-types]
      707 |         count = max(skb->len, ETH_ZLEN);
          |                 ^~~~~~~~~~~~~~~~~~~~~~~
    
    Change-Id: If4a118c06c11ee66b80042534300158ec1f2039b

--- drivers/net/usb/rtl8150.c
+++ drivers/net/usb/rtl8150.c
@@ -695,7 +695,8 @@ static netdev_tx_t rtl8150_start_xmit(struct sk_buff *skb,
 					    struct net_device *netdev)
 {
 	rtl8150_t *dev = netdev_priv(netdev);
-	int count, res;
+	int res;
+	unsigned int count;
 
 	/* pad the frame and ensure terminating USB packet, datasheet 9.2.3 */
 	count = max_t(unsigned int, skb->len, ETH_ZLEN);
